428b0ddfe23c123139f38c1b1124e154cf590760
macbook <macbook@1145141919810.local>
2018-12-12 16:47:36 +0800

    implement Fraction and Matrix class and primarily test them

--- src/main/Matrix.java
+++ src/main/Matrix.java
@@ -1,19 +1,184 @@
 package main;
+import java.util.ArrayList;
 import java.util.Vector;
 
 public class Matrix {
-    private int n;
-    private int m;
-    Vector <Vector<Fraction>> elements = new Vector<>();
-    private int extend_n;
-
-    /*
-        交换第x行和第y行
+    /**
+     * num of rows and cols of the matrix
      */
-    public void exchangeTwoRows(int x, int y){
-        for(int i = 1; i <= this.m; ++i) {
-            int temp;
-            
+    private int rows;
+    private int cols;
+
+    /**
+     * elements of the matrix, which is constructed by Vector
+     */
+    private Vector <Vector<Fraction>> elements = new Vector<>();
+
+    /**
+     * num of cols of the extended matrix
+     */
+    private int extend_cols;
+
+    /**
+     * @apiNote constructor of the matrix (unused)
+     * @param rows: num of rows
+     * @param extend_cols: num of cols of extended matrix
+     */
+    public Matrix(int rows, int cols, int extend_cols){
+        this.rows = rows;
+        this.cols = cols;
+        this.extend_cols = extend_cols;
+    }
+
+    /**
+     * @apiNote constructor of the matrix whose elements are passed by ArrayList
+     * @param rows: num of rows
+     * @param extend_cols: num of cols of extended matrix
+     * @param fractions: num of elements to be passed
+     */
+    public Matrix(int rows, int cols, int extend_cols, ArrayList<Fraction> fractions){
+        this(rows, cols, extend_cols);
+        int size = fractions.size();
+        assert (rows * extend_cols) == size;
+        int index = 0;
+        for (int row = 0; row < rows; row++){
+            Vector<Fraction> newRow = new Vector<>();
+            for (int col = 0; col < extend_cols; col++){
+                newRow.add(fractions.get(index++));
+            }
+            elements.add(newRow);
         }
     }
+
+    /**
+     * @apiNote exchange the elements of two rows
+     * @param row1: the first row
+     * @param row2: the second row
+     */
+    public void exchange(int row1, int row2){
+        for (int col = 0; col < extend_cols; col++){
+            Fraction temp = elements.get(row1).get(col);
+            elements.get(row1).set(col, elements.get(row2).get(col));
+            elements.get(row2).set(col, temp);
+        }
+    }
+
+    /**
+     * @apiNote multiple the elements of srcRow by frac and add them to elements in dstRow
+     * @param srcRow: the source row to be multiplied by frac
+     * @param dstRow: the dst row to be added to
+     * @param frac: the constant frac
+     */
+    public void addOneRowToAnother(int srcRow, int dstRow, Fraction frac){
+        for (int col = 0; col < extend_cols; col++){
+            Fraction tempFrac = elements.get(srcRow).get(col).mul(frac);
+            elements.get(dstRow).set(col, elements.get(dstRow).get(col).add(tempFrac));
+        }
+    }
+
+    /**
+     * @apiNote multiple the elements in row by frac
+     * @param row: the row to be multiplied by frac
+     * @param frac: a constant frac
+     */
+    public void oneRowMul(int row, Fraction frac){
+        for (int col = 0; col < extend_cols; col++){
+            elements.get(row).set(col, elements.get(row).get(col).mul(frac));
+        }
+    }
+
+    /**
+     * @apiNote output the element of matrix
+     */
+    @Override
+    public String toString(){
+        String outputStr = "";
+        for (int row = 0; row < rows; row++){
+            for (int col = 0; col < extend_cols; col++){
+                outputStr += elements.get(row).get(col).toString();
+                outputStr += " ";
+            }
+            outputStr += "\n";
+        }
+        return outputStr;
+    }
+
+    /**
+     * @apiNote judge the matrix is in Row-Echelon form or not
+     */
+    public boolean isEchelon(){
+        int pivotColIndex = findPivot(0);
+        if (pivotColIndex == -1)
+            return false;
+        for (int row = 1; row < rows; row++){
+            int tempPivotColIndex = findPivot(row);
+            if (tempPivotColIndex == -1 || tempPivotColIndex <= pivotColIndex){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * @apiNote judge the matrix is in Reduced-Echelon form or not
+     */
+    public boolean isReducedEchelon(){
+        if (!isEchelon())
+            return false;
+        for (int row = 0; row < rows; row++){
+            int pivot = findPivot(row);
+            assert pivot != -1;
+            if (!elements.get(row).get(pivot).isOne())
+                return false;
+            for (int row1 = 0; row1 < rows; row1++){
+                if (row1 != row && !elements.get(row1).get(pivot).isZero())
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * @apiNote change the pivot to 1 in row
+     * @param row: the row those pivot to be changed to 1
+     */
+    public void changePivotToOne(int row){
+        int pivot = findPivot(row);
+        if (pivot == -1){
+            System.out.println("Warning: the row has no pivot");
+            return;
+        }
+        Fraction pivotFrac = elements.get(row).get(pivot);
+        for (int col = 0; col < extend_cols; col++){
+            if (!elements.get(row).get(col).isZero()){
+                elements.get(row).set(col, elements.get(row).get(col).div(pivotFrac));
+            }
+        }
+    }
+
+    /**
+     * @apiNote get the element in (row, col)
+     * @param row: the row id
+     * @param col: the col id
+     */
+    public Fraction getElement(int row, int col){
+        return elements.get(row).get(col);
+    }
+
+    /**
+     * @apiNote return the pivot of row, if it doesn't exist, return -1
+     * @param row: the row id
+     */
+    public int findPivot(int row){
+        int pivot = -1;
+        for (int col = 0; col < cols; col++){
+            if (!elements.get(row).get(col).isZero()){
+                pivot = col;
+                return pivot;
+            }
+        }
+        return pivot;
+    }
+
+
 }
---
+++ src/main/TestMatrix.java
@@ -0,0 +1,59 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestMatrix {
+
+    public static void main(String[] args){
+        ArrayList<Fraction> fractions = new ArrayList<>();
+        fractions.add(new Fraction("2/3"));
+        fractions.add(new Fraction("1"));
+        fractions.add(new Fraction("1"));
+        fractions.add(new Fraction("1/4"));
+        fractions.add(new Fraction("6"));
+        fractions.add(new Fraction("2"));
+        fractions.add(new Fraction("1"));
+        fractions.add(new Fraction("-1/9"));
+        fractions.add(new Fraction("-2"));
+        fractions.add(new Fraction("2/5"));
+        fractions.add(new Fraction("1"));
+        fractions.add(new Fraction("7"));
+        Matrix matrix = new Matrix(3, 3, 4, fractions);
+        System.out.println(matrix.toString());
+
+        matrix.changePivotToOne(0);
+
+        matrix.addOneRowToAnother(0, 1, new Fraction("-6"));
+        System.out.println(matrix.toString());
+
+        matrix.addOneRowToAnother(0, 2, new Fraction("2"));
+        System.out.println(matrix.toString());
+
+        matrix.changePivotToOne(1);
+
+        matrix.addOneRowToAnother(1, 0, new Fraction("-3/2"));
+        System.out.println(matrix.toString());
+
+        matrix.addOneRowToAnother(1, 2, new Fraction("-17/5"));
+        System.out.println(matrix.toString());
+
+        matrix.changePivotToOne(2);
+        System.out.println(matrix.toString());
+
+        matrix.addOneRowToAnother(2, 0, new Fraction("3/14"));
+        System.out.println(matrix.toString());
+        System.out.println("The above matrix is an echelon? " + matrix.isEchelon());
+        System.out.println("The above matrix is an reduced echelon? " + matrix.isReducedEchelon());
+
+        matrix.addOneRowToAnother(2, 1, new Fraction("-8/7"));
+        System.out.println(matrix.toString());
+        System.out.println("The above matrix is an echelon? " + matrix.isEchelon());
+        System.out.println("The above matrix is an reduced echelon? " + matrix.isReducedEchelon());
+
+
+
+
+
+    }
+}
